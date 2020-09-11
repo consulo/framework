6462e8c52df07d378a2fb28b2149aceb3528eab6
VISTALL <vistall.valeriy@gmail.com>
2020-09-11 13:25:05 +0300

    change groupdId and update to last gwt

--- client-compiler/src/main/java/com/vaadin/tools/ReportUsage.java
+++ client-compiler/src/main/java/com/vaadin/tools/ReportUsage.java
@@ -15,24 +15,12 @@
  */
 package com.vaadin.tools;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
-import java.util.logging.Logger;
-import java.util.prefs.Preferences;
-
-import org.apache.commons.io.IOUtils;
-
-import com.google.gwt.dev.shell.CheckForUpdates;
-import com.vaadin.shared.Version;
 
 public class ReportUsage {
 
-    public static final String ANONYMOUS_ID = loadFirstLaunch();
+    public static final String ANONYMOUS_ID = "";
 
     // for compatibility with old checks
     private static final String USER_AGENT_BASE = "GWT Freshness Checker";
@@ -78,103 +66,6 @@ public class ReportUsage {
     }
 
     public static void report() {
-        long currentTimeMillis = System.currentTimeMillis();
-        Preferences prefs = Preferences.userNodeForPackage(ReportUsage.class);
-        String lastPing = prefs.get(LAST_PING, "0");
-        if (lastPing != null) {
-            try {
-                long lastPingTime = Long.parseLong(lastPing);
-                if (currentTimeMillis < lastPingTime + ONE_DAY) {
-                    return;
-                }
-            } catch (NumberFormatException e) {
-                // error parsing last ping time, ignore and ping
-            }
-        }
-
-        StringBuilder url = new StringBuilder(QUERY_URL);
-        url.append(V_QPARAM);
-        url.append(Version.getFullVersion());
-        url.append(ID_QPARAM);
-        url.append(ANONYMOUS_ID).append(R_QPARAM);
-
-        // TODO add more relevant entry point if feasible
-        String entryPoint = COMPILER;
-
-        if (entryPoint != null) {
-            url.append(E_QPARAM).append(entryPoint);
-        }
-
-        doHttpGet(makeUserAgent(), url.toString());
-
-        prefs.put(LAST_PING, String.valueOf(currentTimeMillis));
+        // stub
     }
-
-    private static void doHttpGet(String userAgent, String url) {
-        Throwable caught;
-        InputStream is = null;
-        try {
-            URL urlToGet = new URL(url);
-            URLConnection conn = urlToGet.openConnection();
-            conn.setRequestProperty(USER_AGENT, userAgent);
-            is = conn.getInputStream();
-            // TODO use the results
-            IOUtils.toByteArray(is);
-            return;
-        } catch (MalformedURLException e) {
-            caught = e;
-        } catch (IOException e) {
-            caught = e;
-        } finally {
-            IOUtils.closeQuietly(is);
-        }
-
-        Logger.getLogger(ReportUsage.class.getName())
-                .fine("Caught an exception while executing HTTP query: "
-                        + caught.getMessage());
-    }
-
-    private static String makeUserAgent() {
-        String userAgent = USER_AGENT_BASE;
-        StringBuilder extra = new StringBuilder();
-        appendUserAgentProperty(extra, "java.vendor");
-        appendUserAgentProperty(extra, "java.version");
-        appendUserAgentProperty(extra, "os.arch");
-        appendUserAgentProperty(extra, "os.name");
-        appendUserAgentProperty(extra, "os.version");
-
-        if (extra.length() != 0) {
-            userAgent += " (" + extra + ")";
-        }
-
-        return userAgent;
-    }
-
-    private static void appendUserAgentProperty(StringBuilder sb,
-            String propName) {
-        String propValue = System.getProperty(propName);
-        if (propValue != null) {
-            if (sb.length() != 0) {
-                sb.append(';').append(' ');
-            }
-            sb.append(propName);
-            sb.append('=');
-            sb.append(propValue);
-        }
-    }
-
-    private static String loadFirstLaunch() {
-        Preferences prefs = Preferences
-                .userNodeForPackage(CheckForUpdates.class);
-
-        String firstLaunch = prefs.get(FIRST_LAUNCH, null);
-        if (firstLaunch == null) {
-            long currentTimeMillis = System.currentTimeMillis();
-            firstLaunch = Long.toHexString(currentTimeMillis);
-            prefs.put(FIRST_LAUNCH, firstLaunch);
-        }
-        return firstLaunch;
-
-    }
-
 }
